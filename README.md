aa7e1a92fee45a3a27cc2c5c7ca743a6fa43bcd0
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-05-07 08:15:32 +0200

    Minor updates.

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # HOWTO
 
 
-By default for 0.9.0 metals version:
+1. Install nix
 
-```bash
-$ nix-channel update
-$ nix-shell --pure https://github.com/kczulko/scala-dev-env/tarball/master
-```
+  ```bash
+  $ curl -L https://nixos.org/nix/install | sh
+  ```
+1. Update nix-channel
+
+  ```bash
+  $ nix-channel update
+  ```
+
+1. Execute (for default metals version)
+
+  ```bash
+  $ nix-shell --pure https://github.com/kczulko/scala-dev-env/tarball/master
+  ```
 
 or with some other metals version
 
-```bash
-$ nix-shell --argstr metalsVersion "X.Y.Z" --pure https://github.com/kczulko/scala-dev-env/tarball/master
-```
+  ```bash
+  $ nix-shell --argstr metalsVersion "X.Y.Z" --pure https://github.com/kczulko/scala-dev-env/tarball/master
+  ```
 
--- default.nix
+++ default.nix
@@ -6,6 +6,8 @@
 
 let
 
+  emacs = pkgs.emacs26;
+
   metals = pkgs.metals.overrideAttrs (oldAttrs: rec {
 
     version = metalsVersion;
@@ -15,12 +17,12 @@ let
     deps = pkgs.stdenv.mkDerivation {
       name = "${baseName}-deps-${version}";
       buildCommand = ''
-            export COURSIER_CACHE=$(pwd)
-            ${pkgs.coursier}/bin/coursier fetch org.scalameta:metals_2.12:${version} \
-               -r bintray:scalacenter/releases \
-               -r sonatype:snapshots > deps
-               mkdir -p $out/share/java
-               cp -n $(< deps) $out/share/java/
+        export COURSIER_CACHE=$(pwd)
+        ${pkgs.coursier}/bin/coursier fetch org.scalameta:metals_2.12:${version} \
+           -r bintray:scalacenter/releases \
+           -r sonatype:snapshots > deps
+           mkdir -p $out/share/java
+           cp -n $(< deps) $out/share/java/
       '';
       outputHashMode = "recursive";
       outputHashAlgo = "sha256";
@@ -34,12 +36,11 @@ let
 
   emacsd-config = pkgs.stdenv.mkDerivation {
     name = "kczulko-emacsd-config";
-    
+
+    buildInputs = [ emacs ];
+
     src = pkgs.fetchgit {
       url = "https://github.com/kczulko/emacsd-config";
-      # owner = "kczulko";
-      # repo = "emacsd-config";
-      # rev = "master";
       deepClone = true;
       sha256 = "13kp7d5d3z2wy6vdwpgbf776rhbfcwnhdqk4gxpln799rbgh468k";
       branchName = "master";
@@ -48,11 +49,8 @@ let
     phases = [ "installPhase" ];
 
     installPhase = ''
-      # mkdir -p ~/.emacsd2
-      # mv $src ~/.emacsd2/
       chmod -R +w $src
-      mkdir -p $out
-      cp -r $src $out
+      ln -sf $src $out/src
     '';
       
   };
@@ -71,6 +69,10 @@ in pkgs.mkShell rec {
     pkgs.emacs26
     pkgs.git
   ];
+
+  shellHook = ''
+    alias emacss='emacs -q --load "${emacsd-config.src}/init.el"'
+  '';
 
   EMACSD_CONFIG=emacsd-config.src;
 }
